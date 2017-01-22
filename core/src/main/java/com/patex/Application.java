19ea3fe22dda11c7c62c562665c82c494816301c
Alex <patexoid@gmail.com>
2017-01-22 21:18:15 +0100

    Add liquibase support

--- core/src/main/java/com/patex/Application.java
+++ core/src/main/java/com/patex/Application.java
@@ -1,5 +1,6 @@
 package com.patex;
 
+import liquibase.integration.spring.SpringLiquibase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -8,6 +9,8 @@ import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 @EnableJpaRepositories
@@ -27,5 +30,15 @@ public class Application {
                 registry.addMapping("/**").allowedOrigins("*");
             }
         };
+    }
+
+    //TODO temporary solution, move it install update flow
+    @Bean
+    public SpringLiquibase liquibase(DataSource dataSource) {
+        SpringLiquibase springLiquibase = new SpringLiquibase();
+        springLiquibase.setDataSource(dataSource);
+        springLiquibase.setChangeLog("classpath:db-changelog.main.xml");
+        return springLiquibase;
+
     }
 }
