795b9c811f74c0724026a6ba108b3f0ba4d1f91e
patexoid <patexoid@gmail.com>
2017-06-26 23:54:21 +0200

    Update ExtLib.java

--- core/src/main/java/com/patex/extlib/ExtLib.java
+++ core/src/main/java/com/patex/extlib/ExtLib.java
@@ -38,7 +38,6 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
