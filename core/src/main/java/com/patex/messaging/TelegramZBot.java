91f97930d55ca77a07c7604779edfd7b31db7082
Alex <patexoid@gmail.com>
2017-06-26 23:00:59 +0200

    Subscription bugfix  -split message  - message only if success of failed downloads

--- core/src/main/java/com/patex/messaging/TelegramZBot.java
+++ core/src/main/java/com/patex/messaging/TelegramZBot.java
@@ -13,6 +13,17 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 
 import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Spliterator;
+import java.util.Spliterators;
+import java.util.StringTokenizer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 /**
  *
@@ -21,18 +32,18 @@ import javax.annotation.PostConstruct;
 @Component
 public class TelegramZBot extends TelegramLongPollingBot implements Messenger {
 
+    public static final int MAX_MESSAGE_SIZE = 4000;
+
     static {
         ApiContextInitializer.init(); //strange magic
     }
 
     private final String botName;
-
+    private final String botToken;
     private String baseurl;
-
     @Autowired
     private MessengerService messagingComponent;
-
-    private final String botToken;
+    private List<String> delims = Arrays.asList("\n", ".", ";", "-", ",");
 
 
     public TelegramZBot(@Value("${telegram.bot.token}") String botToken,
@@ -76,8 +87,65 @@ public class TelegramZBot extends TelegramLongPollingBot implements Messenger {
     @Override
     public void sendToUser(String message, ZUser user) {
         if (user.getUserConfig() != null) {
-            sendMessageToChat(message, user.getUserConfig().getTelegramChatId());
+            List<String> messages = splitMessage(message);
+            for (String s : messages) {
+                sendMessageToChat(s, user.getUserConfig().getTelegramChatId());
+            }
         }
+    }
+
+    private List<String> splitMessage(String message) {
+        List<String> messages = createMessageStream(message, "\n").collect(Collectors.toList());
+        List<String> result = new ArrayList<>(Collections.singleton(""));
+        for (String s : messages) {
+            int lastIndex = result.size() - 1;
+            String lastElem = result.get(lastIndex);
+            if (lastElem.length() + s.length() < MAX_MESSAGE_SIZE) {
+                result.set(lastIndex, lastElem + s);
+            } else {
+                result.add(s);
+            }
+        }
+        return result;
+    }
+
+
+    private Stream<String> createMessageStream(String message, String delim) {
+        if (message.length() < MAX_MESSAGE_SIZE) {
+            return Stream.of(message);
+        } else {
+            StringTokenizer st = new StringTokenizer(message, delim,true);
+            if (st.countTokens() > 1) {
+                return getStream(st).flatMap(s -> createMessageStream(s, delim));
+            } else {
+                int index = delims.indexOf(delim);
+                if (index < delims.size()) {
+                    return createMessageStream(message, delims.get(index) + 1);
+                } else {
+                    Stream<String> first =
+                            createMessageStream(message.substring(0, message.length() / 2), delim);
+                    Stream<String> second =
+                            createMessageStream(message.substring(message.length() / 2), delim);
+                    return Stream.concat(first, second);
+                }
+            }
+        }
+    }
+
+    private Stream<String> getStream(StringTokenizer st) {
+        return StreamSupport.stream(
+                Spliterators.spliteratorUnknownSize(new Iterator<String>() {
+                    @Override
+                    public boolean hasNext() {
+                        return st.hasMoreTokens();
+                    }
+
+                    @Override
+                    public String next() {
+                        return st.nextToken();
+                    }
+                }, Spliterator.ORDERED),
+                false);
     }
 
     private void sendMessageToChat(String message, Long chatId) {
