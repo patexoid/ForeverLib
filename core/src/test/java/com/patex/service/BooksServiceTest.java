6c76dbffc026c0dec7a861c5f4ce60a71921ce48
Alex <patexoid@gmail.com>
2018-01-28 20:56:09 +0100

    book upload refactor and tests

--- core/src/test/java/com/patex/service/BooksServiceTest.java
+++ core/src/test/java/com/patex/service/BooksServiceTest.java
@@ -21,23 +21,18 @@ import java.io.InputStream;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.List;
 import java.util.Optional;
 
-import static org.apache.commons.text.CharacterPredicates.DIGITS;
-import static org.apache.commons.text.CharacterPredicates.LETTERS;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.eq;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.apache.commons.text.CharacterPredicates.*;
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.*;
+import static org.mockito.Mockito.*;
 
 /**
  * Created by Alexey on 15.07.2017.
  */
 public class BooksServiceTest {
-    RandomStringGenerator rsg = new RandomStringGenerator.Builder()
+    private final RandomStringGenerator rsg = new RandomStringGenerator.Builder()
             .withinRange('0', 'z')
             .filteredBy(LETTERS, DIGITS)
             .build();
@@ -49,34 +44,6 @@ public class BooksServiceTest {
     private TransactionService transactionService;
     private ApplicationEventPublisher eventPublisher;
     private BookService bookService;
-    /*
-    @Test
-    public void testFb2Content() throws Exception {
-        BookService bookService = new BookService();
-        ParserService parserService = new ParserService();
-        new ZipFileParser(parserService).register();
-        new Fb2FileParser(parserService).register();
-        bookService.setParserService(parserService);
-
-        FileInputStream is2 = new FileInputStream("C:\\dev\\projects\\ZombieLib2\\storage\\Aromatov_Obektnyy-podhod_1_Obektnyy-podhod.g6s7Nw.196879.fb2.zip");
-
-
-        long currtime = System.currentTimeMillis();
-        int i = 0;
-        for (File f : new File("C:\\dev\\projects\\ZombieLib2\\storage\\").listFiles()) {
-            if (f.isFile() && f.getName().toLowerCase().endsWith("fb2.zip")) {
-                FileInputStream is = new FileInputStream("C:\\dev\\projects\\ZombieLib2\\storage\\Aromatov_Obektnyy-podhod_1_Obektnyy-podhod.9l9iyg.262326.fb2.zip");
-//                boolean shingleComparsion = bookService.shingleComparsion(is, "dsd.fb2.zip", new FileInputStream(f), "dsd.fb2.zip");
-                i++;
-            }
-        }
-
-//        boolean shingleComparsion = bookService.shingleComparsion(is, "dsd.fb2.zip", is2, "dsd.fb2.zip");
-        System.out.println("" + i + " " + (System.currentTimeMillis() - currtime));
-//        Assert.assertTrue(shingleComparsion);
-        ;
-    }
-    */
 
     @Before
     public void setUp() {
@@ -182,5 +149,4 @@ public class BooksServiceTest {
         Book saved = bookService.uploadBook(fileName, bais, new ZUser());
         assertTrue(saved.getSequences().get(0).getSequence() == savedSequence);
     }
-
 }
