d1998bba1060572ec6f6b9dff6add869da13e342
Oleksii <patexoid@gmail.com>
2020-05-31 16:46:39 +0200

    - bugfix in fuzzysearch - improved find same authors books request

--- core/src/main/java/com/patex/entities/BookRepository.java
+++ core/src/main/java/com/patex/entities/BookRepository.java
@@ -2,6 +2,8 @@ package com.patex.entities;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -25,4 +27,13 @@ public interface BookRepository extends org.springframework.data.repository.Repo
     Stream<BookEntity> findAll();
 
     Optional<BookEntity> findById(long id);
+
+    @Query("SELECT b from " +
+            " BookEntity b, " +
+            " AuthorBookEntity ab," +
+            " AuthorBookEntity primaryAB " +
+            "where ab.book=b" +
+            " and ab.author=primaryAB.author" +
+            " and primaryAB.book.id=:bookId")
+    Stream<BookEntity> findSameAuthorBook(@Param("bookId") long bookId);
 }
